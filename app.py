5325aa14ab613ad7244f18bca641de6648147fb6
agaziiantc <131266168+agaziiantc@users.noreply.github.com>
2023-04-20 20:32:05 +0000

    Initial commit

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return render_template("index.html", title="Hello")
+    return """Hello world <br>
+<br> <a href="/1" style="color:rgb(255,0,0)">Goodbye world</a>"""
+@app.route('/1')
+def goodbye_world():
+    return """Goodbye World <br>
+    <br> <a href="/" style="color:rgb(0,255,0)">Hello world</a>"""
+@app.route("/stuypy")
+def stuypy():
+    return render_template('index.html')
+
+app.run()
